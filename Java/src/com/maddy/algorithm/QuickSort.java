128dc37633858da85469e2f063c86e8d5f90276b
madhukar.b <madhukar.b@directi.com>
2016-12-24 18:28:53 +0530

    added quicksort algorithm in java

---
+++ Java/src/com/maddy/algorithm/QuickSort.java
@@ -0,0 +1,55 @@
+package com.maddy.algorithm;
+
+/**
+ * Created by madhukar.b on 24/12/16.
+ */
+public class QuickSort
+{
+    public static void main(String []args)
+    {
+        int[] array = {2, -4, 5, 6, 2, 11, 45, 83, 9823, -8, -33, 34, 54};
+        sort(array);
+
+        for(int item: array)
+        {
+            System.out.println(item);
+        }
+    }
+
+    public static void sort(int[] array)
+    {
+        quickSort(array,0,array.length-1);
+    }
+
+    private static void quickSort(int[] array, int beg, int end)
+    {
+        if(beg<end)
+        {
+            int pivot = partition(array,beg,end);
+            quickSort(array,beg,pivot-1);
+            quickSort(array,pivot+1,end);
+        }
+    }
+
+    private static int partition(int[] array, int beg, int end)
+    {
+        int pivot = array[end];
+        int pivotIndex = beg;
+        int currentIndex = beg;
+
+        while(currentIndex<end)
+        {
+            if(array[currentIndex]< pivot)
+            {
+                int temp = array[currentIndex];
+                array[currentIndex] = array[pivotIndex];
+                array[pivotIndex]=temp;
+                pivotIndex++;
+            }
+            currentIndex++;
+        }
+        array[end] = array[pivotIndex];
+        array[pivotIndex] = pivot;
+        return pivotIndex;
+    }
+}
