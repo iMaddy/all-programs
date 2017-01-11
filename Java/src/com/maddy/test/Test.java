041859dcabc031e20dc2673b07ecc71d68ee380d
maddy <maddy.bangar7@gmail.com>
2017-01-11 12:14:00 +0530

    added algorigthm for prime numbers

--- Java/src/com/maddy/test/Test.java
+++ Java/src/com/maddy/test/Test.java
@@ -1,5 +1,6 @@
 package com.maddy.test;
 
+import com.maddy.algorithm.PrimeNumber;
 import com.maddy.collections.LinkedList;
 
 /**
@@ -9,26 +10,6 @@ public class Test
 {
     public static void main(String[] args)
     {
-        LinkedList list =new LinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.addAtBeginning(0);
-        list.addAtBeginning(-1);
-        list.print();
-////        System.out.println(list.find(2));
-//        //list.swap(32,13);
-//        list.swap(32,13);
-//        list.print();
-//        list.swap(13,32);
-//        list.swap(13,32);
-//        list.print();
-
-        list.reverse();
-        list.print();
-
+    	System.out.println(PrimeNumber.isPrime(23234));
     }
 }
