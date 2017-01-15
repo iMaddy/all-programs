9638b79d2aeae26c031f5d762d87678622e56348
maddy <maddy.bangar7@gmail.com>
2017-01-15 11:06:27 +0530

    Added longest common subsequence algo

---
+++ Java/src/com/maddy/algorithm/LongestCommonSubsequence.java
@@ -0,0 +1,53 @@
+package com.maddy.algorithm;
+
+public class LongestCommonSubsequence
+{
+
+	public static int LCS(String str1, String str2)
+	{
+		//return LongestCommonSubsequence.lcsInternalRecursive(str1, str2, 0, 0);
+		return LongestCommonSubsequence.lcsInternalDynamic(str1, str2);
+	}
+	
+	/*
+	 * recursive approach
+	 * complexity of algorithm: time: O(2^n)
+	 */
+	private static int lcsInternalRecursive(String str1, String str2, int len1, int len2)
+	{
+		if(str1.length() == len1 || str2.length() == len2)
+			return 0;
+		
+		if(str1.charAt(len1) == str2.charAt(len2))
+			return 1 + LongestCommonSubsequence.lcsInternalRecursive(str1, str2, len1+1, len2+1);
+		else
+			return Math.max(
+					LongestCommonSubsequence.lcsInternalRecursive(str1, str2, len1+1, len2), 
+					LongestCommonSubsequence.lcsInternalRecursive(str1, str2, len1, len2+1)
+					);
+	}
+	
+	/*
+	 * dynamic programming
+	 * complexity: time: O(m*n) space: O(m*n)
+	 */
+	
+	private static int lcsInternalDynamic(String str1, String str2)
+	{
+		int array[][] = new int[str1.length()+1][str2.length()+1];
+		
+		for(int i=0; i<=str1.length(); i++)
+		{
+			for(int j=0; j<=str2.length(); j++)
+			{
+				if(i==0 || j==0)
+					array[i][j] = 0;
+				else if(str1.charAt(i-1) == str2.charAt(j-1))
+					array[i][j] = 1 + array[i-1][j-1];
+				else
+					array[i][j] = Math.max(array[i][j-1], array[i-1][j]);
+			}
+		}
+		return array[str1.length()][str2.length()];
+	}
+};
--- Java/src/com/maddy/test/Test.java
+++ Java/src/com/maddy/test/Test.java
@@ -1,6 +1,6 @@
 package com.maddy.test;
 
-import com.maddy.algorithm.Number;
+import com.maddy.algorithm.LongestCommonSubsequence;
 
 /**
  * Created by madhukar.b on 04/09/16.
@@ -9,6 +9,8 @@ public class Test
 {
     public static void main(String[] args)
     {
-    	System.out.println(Number.LCM(105,10));
+    	String str1 = "abcbcab";
+    	String str2 = "abab";
+    	System.out.println(str1 + " " + str2+ " " + LongestCommonSubsequence.LCS(str1, str2));
     }
 }
