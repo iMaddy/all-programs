fe317f074edb4ac07d05e2d3291cafc7bb593131
maddy <maddy.bangar7@gmail.com>
2017-02-17 15:40:24 +0530

    Added Graph data structure

---
+++ Java/src/com/maddy/collections/graph/factory/AbstractGraphFactory.java
@@ -0,0 +1,11 @@
+package com.maddy.collections.graph.factory;
+
+import com.maddy.collections.graph.Graph;
+
+/**
+ * Created by gitanjali on 17/02/17.
+ */
+public abstract class AbstractGraphFactory
+{
+    public abstract Graph createGraph(int v);
+}
