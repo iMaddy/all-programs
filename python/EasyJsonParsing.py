305812a4a3479317be1185a22593150b07265f92
Bangar, Madhukar <mbbanga@amazon.com>
2018-02-22 20:08:56 +0530

    Simple json parsing utility functions to search keys in nested json

---
+++ python/EasyJsonParsing.py
@@ -0,0 +1,17 @@
+# pip install if this module is not installed
+# pip install flatten_json
+from flatten_json import flatten
+import json
+
+def searchDictionaryKey(dict,key):
+    for k in dict:
+            if key in k:
+                    print k
+
+def getJsonKey(flatten_key):
+    keys = flatten_key.split('#')
+    fullKey = ''
+    for key in keys:
+            fullKey += '[\'' + key +'\']'
+    #print fullKey
+    return fullKey
